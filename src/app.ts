36371ce7dc92a2a4208f6e813b7acb56eedf90d0
xakok <xakok@quartsoft.com>
2019-09-07 04:32:27 +0300

    postgres config

--- src/app.ts
+++ src/app.ts
@@ -5,19 +5,10 @@ import createDebug from "debug";
 import express from "express";
 import createLogger from "morgan";
 
-// import {Connection, ConnectionOptions, createConnection} from "typeorm";
+import { ConnectionOptions, createConnection } from "typeorm";
 import router from "./route";
 
 const production = process.env.NODE_ENV === "development";
-
-/*const connectionOptions: ConnectionOptions = {
-    type: "mysql",
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT || 3306),
-    extra: {
-
-    },
-};*/
 
 const app = express();
 app.use(createLogger(production ? "combined" : "dev"));
@@ -25,4 +16,20 @@ app.use(router);
 
 const debug = createDebug("task-api");
 const port = Number(process.env.PORT || 3000);
-app.listen(port, () => debug(`Listening on ${port}`));
+
+const connectionOptions: ConnectionOptions = {
+    type: "postgres",
+    url: process.env.DATABASE_URL,
+    extra: {
+        poolSize: 5,
+    },
+};
+
+createConnection(connectionOptions)
+    .then(() => {
+        app.listen(port, () => debug(`Listening on ${port}`));
+    })
+    .catch((e) => {
+        console.error(e);
+        process.exit(1);
+    });
