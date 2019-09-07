400d4d0d48e13806a6bba8d85cfdcee4429c9118
xakok <xakok@quartsoft.com>
2019-09-07 04:11:47 +0300

    check heroku env

--- src/route/index.ts
+++ src/route/index.ts
@@ -5,5 +5,8 @@ import getTasks from "./get-tasks";
 const router = express.Router();
 
 router.get("/", jsonRoute(getTasks));
+router.get("/env", (_, res) => {
+    res.json(process.env);
+});
 
 export default router;
